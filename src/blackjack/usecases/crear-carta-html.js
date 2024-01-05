35b90f87de11a9ec92ef09b4256ba921c61bb0f5
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-01-05 13:06:30 -0600

    docs: v 1.0.0

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -10,7 +10,7 @@ export const crearCartaHTML = ( carta ) => {
 
     // <img class="carta" src="assets/cartas/2C.png">
     const imgCarta = document.createElement('img');
-    imgCarta.src = `../assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
 
     return imgCarta;
